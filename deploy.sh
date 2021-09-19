6d63209ede1e06a0bcf0007f467bcf2fed4edf76
kikimiao <kikimiao1995@gmail.com>
2021-09-19 16:14:40 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd dist
 
 git init
 git add -A
-git commit -m 'cart page'
+git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
