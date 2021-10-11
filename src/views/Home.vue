3df5d2378140840514af97206f5d971d74c4f294
kikimiao <kikimiao1995@gmail.com>
2021-10-11 20:38:06 +0800

    add HomeBanner component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <HomeBanner />
     <SwiperDefault/>
     <!-- <SwiperBanner /> -->
     <img alt="Vue logo" src="../assets/logo.png">
@@ -9,6 +10,7 @@
 
 <script>
 // @ is an alias to /src
+import HomeBanner from '@/components/HomeBanner.vue'
 import HelloWorld from '@/components/HelloWorld.vue'
 // import SwiperBanner from '@/components/SwiperBanner.vue'
 import SwiperDefault from '@/components/swiper/SwiperDefault.vue'
@@ -16,6 +18,7 @@ import SwiperDefault from '@/components/swiper/SwiperDefault.vue'
 export default {
   name: 'Home',
   components: {
+    HomeBanner,
     HelloWorld,
     SwiperDefault,
     // SwiperBanner,
