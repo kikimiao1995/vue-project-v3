88294f1afad22a009e0a3237e0637900ebfdf108
kikimiao <kikimiao1995@gmail.com>
2021-10-15 20:35:26 +0800

    create ProductCardShow + danamic routes by params

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/products/:id',
+    component: () => import('../views/ProductCardShow.vue')
   }
 ]
 
---
+++ src/views/ProductCardShow.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="ProductCard">
+        <h1> {{ hello }} </h1>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            hello:this.$route.params,
+        }
+    },
+}
+</script>
