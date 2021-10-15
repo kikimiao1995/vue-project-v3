8282dcb1f0e1c379cb005a6eb2b7c455ab137a71
kikimiao <kikimiao1995@gmail.com>
2021-10-15 10:45:48 +0800

    npm install v-pagination-3 and create MyPagination

---
+++ src/components/MyPagination.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="text">
+        我在測試pagination
+    </div>
+    <div class="myList">
+        <div 
+            v-for="list in lists"     
+            :key="list.id">
+            {{ list.name }}
+        </div>
+    </div>
+    <pagination 
+        v-model="page" 
+        :records="20" 
+        :per-page="6" 
+        @paginate="myCallback"/>
+</template>
+<script>
+import Pagination from 'v-pagination-3';
+export default {
+    name:"MyPagination",
+    components: {
+        Pagination
+    },
+    data() {
+        return {
+            page: 1
+        }    
+    }   
+}
+</script>
